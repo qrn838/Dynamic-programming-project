791f667b428bfc728e92d7d3d91f84900cd84e3f
MKildemark <wrc938@econ.ku.dk>
2024-04-27 15:26:16 +0200

    hand to mouth initial attempt martin

---
+++ Funcs.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def consumption_utility(x):
+	v = np.log(x)
+	return v
+
+
+def utility(par,c,r):
+    """ utility function """
+	# a. utility
+    if c>=r:
+       u = consumption_utility(c) + par.eta*(consumption_utility(c)-consumption_utility(r))
+    else:
+       u = consumption_utility(c) + par.eta*par.sigma*(consumption_utility(c)-consumption_utility(r))
+
+    return u
+
+def cost(s):
+	""" cost function """
+	return 5*s**2
---
+++ Model.py
@@ -0,0 +1,100 @@
+from types import SimpleNamespace
+import numpy as np
+import scipy
+from copy import deepcopy
+
+from EconModel import EconModelClass
+
+
+class ReferenceDependenceClass(EconModelClass):
+
+	def settings(self):
+		""" basic settings """
+		
+		self.namespaces = ['par'] # must be numba-able
+		#self.other_attrs = [] 
+
+	def setup(self):
+		""" choose parameters """
+		par = self.par
+		# a. model
+  		
+		par.N = 15 #Number of reference periods
+		# Transfers Structure
+		par.T1 = 10   #Time with high transfers
+		par.T2 = 10   #Time with medium transfers
+		par.T3 = par.N+1 #Time with low transfers
+		par.T = par.T1 + par.T2 + par.T3 #Total number of periods
+		
+        # Income Structure
+		par.w = 1.0     #Normalize wages
+		par.b1 = 0.7*par.w    # High transfers
+		par.b2 = 0.5*par.w    # Medium transfers
+		par.b3 = 0.4*par.w    # Low transfers
+
+		# Preferences
+		par.eta = 1.0
+		par.sigma = 1.5  ### Lambda in the paper
+		par.delta = 0.9  ### Discount factor
+	
+
+		par.Nstates_fixed = 0 # number of fixed states
+		par.Nstates_fixed_pd = 0 # number of fixed post-decision states
+		par.Nstates_dynamic = 2 # number of dynamic states (Employed/Unemployed)
+		par.Nstates_dynamic_pd = 2 # number of dynamic post-decision states (Employed/Unemployed)
+		par.Nactions = 1 # number of actions (Search effort)
+	
+		
+
+	def allocate(self):
+		""" allocate arrays  """
+
+		# a. unpack
+		par = self.par
+		
+        #Income when unemployed
+		par.income_u = np.zeros(par.T) 
+		par.income_u[0:par.T1] = par.b1
+		par.income_u[par.T1:par.T1+par.T2] = par.b2
+		par.income_u[par.T1+par.T2:] = par.b3
+	
+        #Income when employed
+		par.income_e = np.zeros((par.T, par.T))
+		for t in range(par.T):
+			par.income_e[t, :] = par.income_u
+			par.income_e[t, t:] = par.w
+	
+	
+		# Reference points unemployed
+		par.r_u = np.zeros(par.T)	
+		par.ref_income_u = np.zeros(par.T+par.N)
+		par.ref_income_u[0:par.N] = par.w
+		par.ref_income_u[par.N:] = par.income_u
+		
+		for t in range(par.T):
+			par.r_u[t] = par.ref_income_u[t:t+par.N].mean()
+	
+		# Reference points employed
+		par.r_e = np.zeros((par.T, par.T+par.N))
+		par.ref_income_e = np.zeros((par.T, par.T+2*par.N))
+		for t in range(par.T):
+			par.ref_income_e[t, 0:par.N] = par.w
+			par.ref_income_e[t, par.N:par.T+par.N] = par.income_e[t, :]
+			par.ref_income_e[t, par.T+par.N:] = par.w
+			for s in range(par.T+par.N):
+				par.r_e[t, s] = par.ref_income_e[t, s:s+par.N].mean()
+		
+		#reference point for next ten periods
+		par.r_e_future = np.zeros((par.T, par.N))
+		for t in range(par.T):
+			par.r_e_future[t, :] = par.r_e[t, t:t+par.N]
+	
+
+		# b. states
+		par.Nstates = par.Nstates_dynamic + par.Nstates_fixed # number of states
+		par.Nstates_pd = par.Nstates_dynamic_pd + par.Nstates_fixed_pd # number of post-decision states
+
+		par.Nstates_t = par.T # number of auxiliary states
+		par.Nstates_pd_t = par.T # number of auxiliary post-decision states
+		
+
