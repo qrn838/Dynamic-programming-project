8206c0c47b400d91ac06a3c04b0b2b617345ed30
MKildemark <wrc938@econ.ku.dk>
2024-05-28 13:57:53 +0200

    ready for new estimation attempt

--- Model.py
+++ Model.py
@@ -108,14 +108,15 @@ class ReferenceDependenceClass(EconModelClass):
 		####		   Initial guesses 		    ####
 		############################################
 
-		par.noOfParams = 7				# Number of parameters
+
 		par.noSearchInits = 30			# Number of numerical minimizations to run
 
 		# The range from initial values are drawn (The same as in DellaVigna et al. (2017))
-		par.lb_rep = np.zeros(par.noOfParams)
-		par.ub_rep = np.ones(par.noOfParams)
+		# Search cost 1 type
+		par.lb_c = 200
+		par.ub_c = 400
 		# Highest search cost
-		par.lb_hsc = 50
+		par.lb_hsc = 100
 		par.ub_hsc = 1000
 		# Medium search cost
 		par.lb_msc = 30
@@ -124,20 +125,26 @@ class ReferenceDependenceClass(EconModelClass):
 		par.lb_lsc = 0
 		par.ub_lsc = 100
 		# Gamma
-		par.lb_gam = 0.1
-		par.ub_gam = 1.3
+		par.lb_gamma = 0.1
+		par.ub_gamma = 1.3
 		# Shares of types
-		par.lb_share = 0
+		par.lb_share = 0.1
 		par.ub_share = 2/par.types
-		# Sigma
-		par.lb_sig = 1
-		par.ub_sig = 30
+		# Lambdaa
+		par.lb_lambdaa = 1
+		par.ub_lambdaa = 5
 		# N
-		par.lb_N = 1
-		par.ub_N = 25
+		par.lb_N = 10
+		par.ub_N = 20
 		# Welfare
-		par.lb_wel = 0
-		par.ub_wel = 200/450
+		par.lb_welfare = 0
+		par.ub_welfare = 200/450
+		# Delta
+		par.lb_delta = 0.7
+		par.ub_delta = 0.999
+		# L
+		par.lb_L = -0.9
+		par.ub_L = 0.0
 
 		################################################
 		# Needed for EconModelClass (not used)
@@ -164,35 +171,7 @@ class ReferenceDependenceClass(EconModelClass):
 
 		par.type_shares2 = 1-par.type_shares1 - par.type_shares3
 
-		# Initial guesses - reference dependent model
-		if par.eta == 1:
-			par.lb_rep[0] = par.lb_msc
-			par.ub_rep[0] = par.ub_msc
-			par.lb_rep[1] = par.lb_lsc
-			par.ub_rep[1] = par.ub_lsc
-			par.lb_rep[2] = par.lb_gam
-			par.ub_rep[2] = par.ub_gam
-			par.lb_rep[3] = par.lb_share
-			par.ub_rep[3] = par.ub_share
-			par.lb_rep[4] = par.lb_sig
-			par.ub_rep[4] = par.ub_sig
-			par.lb_rep[5] = par.lb_N
-			par.ub_rep[5] = par.ub_N
-			par.lb_rep[6] = par.lb_wel
-			par.ub_rep[6] = par.ub_wel
-		else:
-			par.lb_rep[0] = par.lb_hsc
-			par.ub_rep[0] = par.ub_hsc
-			par.lb_rep[1] = par.lb_msc
-			par.ub_rep[1] = par.ub_msc
-			par.lb_rep[2] = par.lb_lsc
-			par.ub_rep[2] = par.ub_lsc
-			par.lb_rep[3] = par.lb_gam
-			par.ub_rep[3] = par.ub_gam
-			par.lb_rep[4:6] = par.lb_share
-			par.ub_rep[4:6] = par.ub_share
-			par.lb_rep[6] = par.lb_wel
-			par.ub_rep[6] = par.ub_wel
+	
 
         #Income when unemployed
 		par.income_u = np.zeros(par.T)					# Empty array to store benefits
@@ -286,6 +265,7 @@ class ReferenceDependenceClass(EconModelClass):
 		return sim_s
 	
 	def solve(self):
+		""" solve the model """
 		if self.par.model == 'ConSav':
 			sim_s = self.solve_ConSav()
 		elif self.par.model == 'HTM':
